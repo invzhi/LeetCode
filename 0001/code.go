13c2b742f74b44350ccd1bf12f9a5711f4b17d74
invzhi <invzhi@protonmail.com>
2017-11-18 21:34:25 +0800

    Modify for more readable

--- 0001/code.go
+++ 0001/code.go
@@ -2,13 +2,14 @@ package leetcode
 
 func twoSum(nums []int, target int) []int {
 	m := make(map[int]int)
+	var indices []int
 
 	for i, num := range nums {
 		if v, isExist := m[target-num]; isExist {
-			return []int{i, v}
+			indices = []int{i, v}
 		} else {
 			m[num] = i
 		}
 	}
-	return nil
+	return indices
 }
