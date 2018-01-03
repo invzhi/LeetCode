654067bbba5f21bbb4f55216914cff93720e6245
invzhi <invzhi@protonmail.com>
2018-01-03 10:37:47 +0800

    Add problem 653: Two Sum IV - Input is a BST

---
+++ 0653/code.go
@@ -0,0 +1,45 @@
+// Given a Binary Search Tree and a target number, return true if there exist two elements in the BST such that their sum is equal to the given target.
+
+// Example 1:
+// Input:
+//     5
+//    / \
+//   3   6
+//  / \   \
+// 2   4   7
+// Target = 9
+// Output: True
+
+// Example 2:
+// Input:
+//     5
+//    / \
+//   3   6
+//  / \   \
+// 2   4   7
+// Target = 28
+// Output: False
+
+package leetcode
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func findTarget(root *TreeNode, k int) bool {
+	m := make(map[int]struct{})
+	return search(root, k, m)
+}
+
+func search(node *TreeNode, k int, m map[int]struct{}) bool {
+	if node == nil {
+		return false
+	}
+	if _, ok := m[k-node.Val]; ok {
+		return true
+	}
+	m[node.Val] = struct{}{}
+	return search(node.Left, k, m) || search(node.Right, k, m)
+}
---
+++ 0653/code_test.go
@@ -0,0 +1,52 @@
+package leetcode
+
+import "testing"
+
+func insertLeaf(root, leaf *TreeNode) {
+	for root != nil {
+		switch {
+		case root.Val < leaf.Val:
+			if root.Right == nil {
+				root.Right = leaf
+				return
+			}
+			root = root.Right
+		case root.Val > leaf.Val:
+			if root.Left == nil {
+				root.Left = leaf
+				return
+			}
+			root = root.Left
+		}
+	}
+}
+
+func buildTree(nums []int) *TreeNode {
+	root := &TreeNode{Val: nums[0]}
+	l := len(nums)
+
+	for i := 1; i < l; i++ {
+		leaf := &TreeNode{Val: nums[i]}
+		insertLeaf(root, leaf)
+	}
+	return root
+}
+
+func TestFindTarget(t *testing.T) {
+	var tests = []struct {
+		nums   []int
+		target int
+		res    bool
+	}{
+		{[]int{5, 3, 6, 2, 4, 7}, 9, true},
+		{[]int{5, 3, 6, 2, 4, 7}, 28, false},
+	}
+
+	for _, tt := range tests {
+		tree := buildTree(tt.nums)
+		res := findTarget(tree, tt.target)
+		if res != tt.res {
+			t.Errorf("findTarget(%v, %v) return %v, want %v", tt.nums, tt.target, res, tt.res)
+		}
+	}
+}
