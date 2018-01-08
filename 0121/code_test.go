069ea5945924e8a5255a56bef35babb7dde3e501
invzhi <invzhi@protonmail.com>
2018-01-08 22:42:33 +0800

    Problem 121: add solution

---
+++ 0121/code_test.go
@@ -0,0 +1,20 @@
+package leetcode
+
+import "testing"
+
+func TestMaxProfit(t *testing.T) {
+	var tests = []struct {
+		prices []int
+		profit int
+	}{
+		{[]int{7, 1, 5, 3, 6, 4}, 5},
+		{[]int{7, 6, 4, 3, 1}, 0},
+	}
+
+	for _, tt := range tests {
+		profit := maxProfit(tt.prices)
+		if profit != tt.profit {
+			t.Errorf("maxProfit(%v) return %v, want %v", tt.prices, profit, tt.profit)
+		}
+	}
+}
