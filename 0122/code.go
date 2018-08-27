ede51852a9fa2f2ef3b158d651138225eb201286
invzhi <invzhi@protonmail.com>
2018-08-27 21:23:07 +0800

    Problem 122: reduce slice access times

--- 0122/code.go
+++ 0122/code.go
@@ -2,17 +2,35 @@
 
 // Say you have an array for which the ith element is the price of a given stock on day i.
 
-// Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times). However, you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+// Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+
+// Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+
+// Example 1:
+// Input: [7,1,5,3,6,4]
+// Output: 7
+// Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4. Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+
+// Example 2:
+// Input: [1,2,3,4,5]
+// Output: 4
+// Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4. Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are engaging multiple transactions at the same time. You must sell before buying again.
+
+// Example 3:
+// Input: [7,6,4,3,1]
+// Output: 0
+// Explanation: In this case, no transaction is done, i.e. max profit = 0.
 
 package leetcode
 
 func maxProfit(prices []int) int {
-	profit := 0
-	l := len(prices)
+	var profit int
+	pl := len(prices)
 
-	for i := 1; i < l; i++ {
-		if prices[i] > prices[i-1] {
-			profit += prices[i] - prices[i-1]
+	for i := 1; i < pl; i++ {
+		p := prices[i] - prices[i-1]
+		if p > 0 {
+			profit += p
 		}
 	}
 	return profit
