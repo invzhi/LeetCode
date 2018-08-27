afcab78973389f64af1abf66d2c3402dc2c60eaf
invzhi <invzhi@protonmail.com>
2018-08-27 20:59:25 +0800

    Problem 121: rename some variable

--- 0121/code.go
+++ 0121/code.go
@@ -2,29 +2,30 @@
 
 // Say you have an array for which the ith element is the price of a given stock on day i.
 // If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+// Note that you cannot sell a stock before you buy one.
 
 // Example 1:
 // Input: [7, 1, 5, 3, 6, 4]
 // Output: 5
-// max. difference = 6-1 = 5 (not 7-1 = 6, as selling price needs to be larger than buying price)
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5. Not 7-1 = 6, as selling price needs to be larger than buying price.
 
 // Example 2:
 // Input: [7, 6, 4, 3, 1]
 // Output: 0
-// In this case, no transaction is done, i.e. max profit = 0.
+// Explanation: In this case, no transaction is done, i.e. max profit = 0.
 
 package leetcode
 
 func maxProfit(prices []int) int {
-	minI, maxP := 0, 0
+	var minPriceI, max int
 
 	for i, price := range prices {
-		profit := price - prices[minI]
-		if profit < 0 {
-			minI = i
-		} else if profit > maxP {
-			maxP = profit
+		profit := price - prices[minPriceI]
+		if profit > max {
+			max = profit
+		} else if profit < 0 {
+			minPriceI = i
 		}
 	}
-	return maxP
+	return max
 }
