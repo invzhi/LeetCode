3eb48d4ede4eaa57bcc0e0eef1f784dd1160b2a0
invzhi <invzhi@protonmail.com>
2018-08-28 21:09:23 +0800

    Problem 167: rm extra solution

--- 0167/code.go
+++ 0167/code.go
@@ -2,26 +2,18 @@
 
 // Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
 // The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+// Note:
+// - Your returned answers (both index1 and index2) are not zero-based.
+// - You may assume that each input would have exactly one solution and you may not use the same element twice.
 
-// You may assume that each input would have exactly one solution and you may not use the same element twice.
-// Input: numbers={2, 7, 11, 15}, target=9
-// Output: index1=1, index2=2
+// Example:
+// Input: numbers = [2,7,11,15], target = 9
+// Output: [1,2]
+// Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
 
 package leetcode
 
 func twoSum(numbers []int, target int) []int {
-	m := make(map[int]int)
-
-	for i, num := range numbers {
-		if j, ok := m[target-num]; ok {
-			return []int{j + 1, i + 1}
-		}
-		m[num] = i
-	}
-	return nil
-}
-
-func twoSum1(numbers []int, target int) []int {
 	l, r := 0, len(numbers)-1
 
 	for l < r {
--- 0167/code_test.go
+++ 0167/code_test.go
@@ -5,36 +5,22 @@ import (
 	"testing"
 )
 
-type twoSumFunc struct {
-	f    func([]int, int) []int
-	name string
-}
-
-func testTwoSum(t *testing.T, f twoSumFunc) {
+func TestTwoSum(t *testing.T) {
 	var tests = []struct {
 		numbers []int
 		target  int
 		indices []int
 	}{
 		{[]int{2, 7, 11, 15}, 9, []int{1, 2}},
-		{[]int{2, 5, 6, 7}, 12, []int{2, 4}},
-		{[]int{2, 6, 8, 23}, 16, nil},
-		{[]int{2}, 16, nil},
-		{nil, 16, nil},
+		{[]int{2, 3, 4, 6}, 6, []int{1, 3}},
+		{[]int{2, 3, 4, 7}, 8, nil},
+		{nil, 8, nil},
 	}
 
 	for _, tt := range tests {
-		indices := f.f(tt.numbers, tt.target)
+		indices := twoSum(tt.numbers, tt.target)
 		if reflect.DeepEqual(indices, tt.indices) == false {
-			t.Errorf("%v(%v, %v) return %v, want %v", f.name, tt.numbers, tt.target, indices, tt.indices)
+			t.Errorf("twoSum(%v, %v) return %v, want %v", tt.numbers, tt.target, indices, tt.indices)
 		}
 	}
-}
-
-func TestTwoSum(t *testing.T) {
-	testTwoSum(t, twoSumFunc{f: twoSum, name: "twoSum"})
-}
-
-func TestTwoSum1(t *testing.T) {
-	testTwoSum(t, twoSumFunc{f: twoSum1, name: "twoSum"})
 }
