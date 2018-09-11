f48e2eb04387882c58d72abd0e7f28a42da3a780
invzhi <invzhi@protonmail.com>
2018-09-11 21:33:40 +0800

    Problem 111: add solution

---
+++ 0111/code.go
@@ -0,0 +1,43 @@
+// 111. Minimum Depth of Binary Tree
+
+// Given a binary tree, find its minimum depth.
+// The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+// Note: A leaf is a node with no children.
+
+// Example: Given binary tree [3,9,20,null,null,15,7], 3
+//    / \
+//   9  20
+//     /  \
+//    15   7
+// return its depth = 2.
+
+package leetcode
+
+// TreeNode is node of binary tree
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	if root.Left == nil {
+		return minDepth(root.Right) + 1
+	}
+	if root.Right == nil {
+		return minDepth(root.Left) + 1
+	}
+
+	ld := minDepth(root.Left)
+	rd := minDepth(root.Right)
+
+	d := ld
+	if rd < ld {
+		d = rd
+	}
+	return d + 1
+}
