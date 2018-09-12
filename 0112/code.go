a66b0ca768231ec5be5e4b0a778a434772fba279
invzhi <invzhi@protonmail.com>
2018-09-12 20:47:38 +0800

    Problem 112: add solution

---
+++ 0112/code.go
@@ -0,0 +1,36 @@
+// 112. Path Sum
+
+// Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+// Note: A leaf is a node with no children.
+
+// Example:
+// Given the below binary tree and sum = 22,
+//       5
+//      / \
+//     4   8
+//    /   / \
+//   11  13  4
+//  /  \      \
+// 7    2      1
+// return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+
+package leetcode
+
+// TreeNode is node of binary tree
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func hasPathSum(root *TreeNode, sum int) bool {
+	if root == nil {
+		return false
+	}
+	if root.Left == nil && root.Right == nil {
+		return sum == root.Val
+	}
+
+	sum -= root.Val
+	return hasPathSum(root.Left, sum) || hasPathSum(root.Right, sum)
+}
