6d889a383f18470104c1f3b8ca411b729f79c8cb
invzhi <invzhi@protonmail.com>
2018-09-27 20:56:16 +0800

    Problem 148: add solution

---
+++ 0148/code.go
@@ -0,0 +1,68 @@
+// 148. Sort List
+
+// Sort a linked list in O(n log n) time using constant space complexity.
+
+// Example 1:
+// Input: 4->2->1->3
+// Output: 1->2->3->4
+
+// Example 2:
+// Input: -1->5->3->4->0
+// Output: -1->0->3->4->5
+
+package leetcode
+
+// ListNode is node of linked list
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func sortList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	m := findMiddle(head)
+
+	r := sortList(m.Next)
+	m.Next = nil
+	l := sortList(head)
+
+	return merge(l, r)
+}
+
+func findMiddle(head *ListNode) *ListNode {
+	slow, fast := head, head.Next
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return slow
+}
+
+func merge(l1, l2 *ListNode) *ListNode {
+	l := new(ListNode)
+
+	n := l
+	for l1 != nil && l2 != nil {
+		if l1.Val <= l2.Val {
+			n.Next = l1
+			n = n.Next
+			l1 = l1.Next
+		} else {
+			n.Next = l2
+			n = n.Next
+			l2 = l2.Next
+		}
+	}
+
+	if l1 != nil {
+		n.Next = l1
+	}
+	if l2 != nil {
+		n.Next = l2
+	}
+
+	return l.Next
+}
---
+++ 0148/code_test.go
@@ -0,0 +1,41 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSortList(t *testing.T) {
+	var tests = []struct {
+		list       []int
+		sortedList *ListNode
+	}{
+		{
+			[]int{4, 2, 1, 3},
+			newList(1, 2, 3, 4),
+		},
+		{
+			[]int{-1, 5, 3, 4, 0},
+			newList(-1, 0, 3, 4, 5),
+		},
+		{
+			[]int{1, 2, 3, 4},
+			newList(1, 2, 3, 4),
+		},
+		{
+			[]int{4, 3, 2, 1},
+			newList(1, 2, 3, 4),
+		},
+		{
+			nil,
+			nil,
+		},
+	}
+
+	for _, tt := range tests {
+		sortedList := sortList(newList(tt.list...))
+		if reflect.DeepEqual(sortedList, tt.sortedList) == false {
+			t.Errorf("sortList(%v) return %v, want %v", newList(tt.list...), sortedList, tt.sortedList)
+		}
+	}
+}
