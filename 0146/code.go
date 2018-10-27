4bdff3f4bff54774094a75bc84b7bec17311a10f
invzhi <invzhi@protonmail.com>
2018-10-27 18:52:20 +0800

    Problem 146: add solution

---
+++ 0146/code.go
@@ -0,0 +1,67 @@
+// 146. LRU Cache
+
+// Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and put.
+// get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+// put(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+
+// Follow up:
+// Could you do both operations in O(1) time complexity?
+
+// Example:
+// LRUCache cache = new LRUCache( 2 /* capacity */ );
+// cache.put(1, 1);
+// cache.put(2, 2);
+// cache.get(1);       // returns 1
+// cache.put(3, 3);    // evicts key 2
+// cache.get(2);       // returns -1 (not found)
+// cache.put(4, 4);    // evicts key 1
+// cache.get(1);       // returns -1 (not found)
+// cache.get(3);       // returns 3
+// cache.get(4);       // returns 4
+
+package leetcode
+
+import "container/list"
+
+type keyValue struct {
+	key, value int
+}
+
+type LRUCache struct {
+	l *list.List
+	m map[int]*list.Element
+	c int
+}
+
+func Constructor(capacity int) LRUCache {
+	return LRUCache{
+		l: list.New(),
+		m: make(map[int]*list.Element),
+		c: capacity,
+	}
+}
+
+func (c *LRUCache) Get(key int) int {
+	e, ok := c.m[key]
+	if !ok {
+		return -1
+	}
+	c.l.MoveToFront(e)
+	return e.Value.(*keyValue).value
+}
+
+func (c *LRUCache) Put(key, value int) {
+	e, ok := c.m[key]
+	if ok {
+		c.l.MoveToFront(e)
+		e.Value.(*keyValue).value = value
+	} else {
+		e = c.l.PushFront(&keyValue{key, value})
+		c.m[key] = e
+		if c.l.Len() > c.c {
+			oldestElem := c.l.Back()
+			c.l.Remove(oldestElem)
+			delete(c.m, oldestElem.Value.(*keyValue).key)
+		}
+	}
+}
---
+++ 0146/code_test.go
@@ -0,0 +1,34 @@
+package leetcode
+
+import "testing"
+
+func TestLRUCache(t *testing.T) {
+	var tests = []struct {
+		put        bool
+		key, value int
+	}{
+		{true, 1, 1},
+		{true, 2, 2},
+		{false, 1, 1},
+		{true, 3, 3},
+		{false, 2, -1},
+		{true, 4, 4},
+		{false, 1, -1},
+		{false, 3, 3},
+		{false, 4, 4},
+	}
+
+	cache := Constructor(2)
+	for _, tt := range tests {
+		if tt.put {
+			cache.Put(tt.key, tt.value)
+			t.Logf("cache.Put(%v, %v)\n", tt.key, tt.value)
+		} else {
+			value := cache.Get(tt.key)
+			t.Logf("cache.Get(%v) return %v\n", tt.key, value)
+			if value != tt.value {
+				t.Errorf("want %v", tt.value)
+			}
+		}
+	}
+}
