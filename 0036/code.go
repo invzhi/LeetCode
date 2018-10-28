85e37fb50f32b657beeb6230194d1b66a60ff07b
invzhi <invzhi@protonmail.com>
2018-10-28 21:54:13 +0800

    Problem 36: add solution

---
+++ 0036/code.go
@@ -0,0 +1,65 @@
+// 36. Valid Sudoku
+
+// Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+// 1. Each row must contain the digits 1-9 without repetition.
+// 2. Each column must contain the digits 1-9 without repetition.
+// 3. Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+// The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+
+// Example 1:
+// Input:
+// [
+//   ["5","3",".",".","7",".",".",".","."],
+//   ["6",".",".","1","9","5",".",".","."],
+//   [".","9","8",".",".",".",".","6","."],
+//   ["8",".",".",".","6",".",".",".","3"],
+//   ["4",".",".","8",".","3",".",".","1"],
+//   ["7",".",".",".","2",".",".",".","6"],
+//   [".","6",".",".",".",".","2","8","."],
+//   [".",".",".","4","1","9",".",".","5"],
+//   [".",".",".",".","8",".",".","7","9"]
+// ]
+// Output: true
+
+// Example 2:
+// Input:
+// [
+//   ["8","3",".",".","7",".",".",".","."],
+//   ["6",".",".","1","9","5",".",".","."],
+//   [".","9","8",".",".",".",".","6","."],
+//   ["8",".",".",".","6",".",".",".","3"],
+//   ["4",".",".","8",".","3",".",".","1"],
+//   ["7",".",".",".","2",".",".",".","6"],
+//   [".","6",".",".",".",".","2","8","."],
+//   [".",".",".","4","1","9",".",".","5"],
+//   [".",".",".",".","8",".",".","7","9"]
+// ]
+// Output: false
+// Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+
+// Note:
+// - A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+// - Only the filled cells need to be validated according to the mentioned rules.
+// - The given board contain only digits 1-9 and the character '.'.
+// - The given board size is always 9x9.
+
+package leetcode
+
+func isValidSudoku(board [][]byte) bool {
+	var row, col, box [9][10]bool
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			if board[i][j] == '.' {
+				continue
+			}
+			cell := board[i][j] - '0'
+			if row[i][cell] || col[j][cell] || box[i/3*3+j/3][cell] {
+				return false
+			}
+			row[i][cell] = true
+			col[j][cell] = true
+			box[i/3*3+j/3][cell] = true
+		}
+	}
+	return true
+}
