24ac45f2245c7790394f685d23ad9b93dea973c5
invzhi <invzhi@protonmail.com>
2018-11-09 13:01:06 +0800

    Problem 179: fix typo seperately -> separately

--- 0475/code.go
+++ 0475/code.go
@@ -2,7 +2,7 @@
 
 // Winter is coming! Your first job during the contest is to design a standard heater with fixed warm radius to warm all the houses.
 // Now, you are given positions of houses and heaters on a horizontal line, find out minimum radius of heaters so that all houses could be covered by those heaters.
-// So, your input will be the positions of houses and heaters seperately, and your expected output will be the minimum radius standard of heaters.
+// So, your input will be the positions of houses and heaters separately, and your expected output will be the minimum radius standard of heaters.
 
 // Note:
 // 1. Numbers of houses and heaters you are given are non-negative and will not exceed 25000.
