a8348389c4077669a1702ea41179f0e343227a81
invzhi <invzhi@protonmail.com>
2018-12-28 15:03:27 +0800

    Problem 669: fix id in code.go

--- 0669/code.go
+++ 0669/code.go
@@ -1,4 +1,4 @@
-// 69. Trim a Binary Search Tree
+// 669. Trim a Binary Search Tree
 
 // Given a binary search tree and the lowest and highest boundaries as L and R, trim the tree so that all its elements lies in [L, R] (R >= L). You might need to change the root of the tree, so the result should return the new root of the trimmed binary search tree.
 
